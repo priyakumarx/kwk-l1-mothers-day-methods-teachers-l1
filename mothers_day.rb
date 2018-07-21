5cc3d386d320ddb9a571f55616d2f5a9f214e9da
Priya Kumar <priya.kumar08@yahoo.com>
2018-07-21 00:34:59 +0000

    automatically backed up by learn

--- mothers_day.rb
+++ mothers_day.rb
@@ -1,7 +1,6 @@
 ## Define your method, mothers_day, below. Go through the README and update your method as needed!
-def mothers_day("name")
-  print "Happy Mother's Day, Mom! "
-end 
+def mothers_day('name')
+  print "Happy Mother's Day, Mom!"
+end
 
 mothers_day("Beyonce")
-
